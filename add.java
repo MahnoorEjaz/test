791a45c76748bd5755d193065c182f4ed4eef9fe
MahnoorEjaz <ejazmahnoor8@gmail.com>
2025-09-09 10:27:39 +0500

    add function

---
+++ add.java
@@ -0,0 +1,16 @@
+public class AddExample {
+    
+    // Function to add two integers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = add(num1, num2);
+
+        System.out.println("The sum is: " + result);
+    }
+}
