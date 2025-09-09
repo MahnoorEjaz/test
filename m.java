ef7d6fc200d313b6fecda2c264e185b144f5a63d
MahnoorEjaz <ejazmahnoor8@gmail.com>
2025-09-09 10:32:04 +0500

    multiply

---
+++ m.java
@@ -0,0 +1,8 @@
+ public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = add(num1, num2);
+
+        System.out.println("The sum is: " + result);
+    }
